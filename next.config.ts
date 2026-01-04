756012f543aab32fbfb56d9615fa6eb2d770a983
Ricter Z <ricterzheng@gmail.com>
2026-01-04 15:11:58 +0800

    Add allowedDevOrigins configuration and update search routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    'localhost',
+    '127.0.0.1',
+    '192.168.13.100',
+    '192.168.13.2',
+  ],
 };
 
 export default nextConfig;
